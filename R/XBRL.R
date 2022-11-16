592b9ed07f234b282388ded4614f46abcf9dc50f
Riaz Arbi <riazarbi@users.noreply.github.com>
2022-11-16 12:14:43 +0200

    Update XBRL.R

--- R/XBRL.R
+++ R/XBRL.R
@@ -29,7 +29,7 @@ XBRL <- function() {
   inst.lnkb <- NULL
   
   fixFileName <- function(dname, file.name) {
-    if (substr(file.name, 1, 5) != "http:" ) {
+    if (!(substr(file.name, 1, 5) %in% c("http:", "https" ))) {
       if (substr(file.name, 1, 5) == "../..") { ## A better solution is preferred, but it works for now
         file.name <- paste0(dirname(dirname(dname)), "/",  substr(file.name, 7, nchar(file.name)))
       } else if (substr(file.name, 1, 2) == "..") {
@@ -37,16 +37,7 @@ XBRL <- function() {
       } else {
         file.name <- paste0(dname,"/", file.name)
       }
-    } else if (substr(file.name, 1, 6) != "https:" ) {
-      if (substr(file.name, 1, 5) == "../..") { ## A better solution is preferred, but it works for now
-        file.name <- paste0(dirname(dirname(dname)), "/",  substr(file.name, 7, nchar(file.name)))
-      } else if (substr(file.name, 1, 2) == "..") {
-        file.name <- paste0(dirname(dname), "/", substr(file.name, 4, nchar(file.name)))
-      } else {
-        file.name <- paste0(dname,"/", file.name)
-      }
-      }
-    
+    }
     return(file.name)
   }
 
